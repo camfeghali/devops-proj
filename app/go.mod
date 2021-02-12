f13d70452e1383236d8d4246f444971308baad0f
Camille Feghali <camfeg@gmail.com>
2021-02-12 13:06:28 +0100

    enabled cors

--- app/go.mod
+++ app/go.mod
@@ -5,7 +5,10 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/denisenkom/go-mssqldb v0.9.0
+	github.com/go-chi/chi v3.3.2+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.1.0
 	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
--- app/server.go
+++ app/server.go
@@ -1,19 +1,38 @@
 package main
 
 import (
-	"hackernews-clone/graph"
-	"hackernews-clone/graph/generated"
+	"os"
 	"log"
 	"net/http"
-	"os"
+	"hackernews-clone/graph"
 	"hackernews-clone/database"
+	"hackernews-clone/graph/generated"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/gorilla/websocket"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+
+
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -22,10 +41,24 @@ func main() {
 	database.InitDB()
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+    
+	srv.AddTransport(&transport.Websocket{
+        Upgrader: websocket.Upgrader{
+            CheckOrigin: func(r *http.Request) bool {
+                // Check against your desired domains here
+                 return r.Host == "example.org"
+            },
+            ReadBufferSize:  1024,
+            WriteBufferSize: 1024,
+        },
+    })
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL Playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
+	err := http.ListenAndServe(":8080", router)
+    if err != nil {
+        panic(err)
+    }
 }
